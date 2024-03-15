980e37fa88b446106b048bd75823c286ccaade26
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2024-03-15 08:21:53 +0100

    Update erlpool_app.erl

--- src/erlpool_app.erl
+++ src/erlpool_app.erl
@@ -5,7 +5,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    ok = erlpool_compile:compile_settings([]),
+    % ok = erlpool_compile:compile_settings([]),
     ok = erlpool_manager:init(),
     {ok, Pid} = erlpool_sup:start_link(),
     start_pools(),
