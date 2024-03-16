2e58d9e06926b282559f3f2d8742048034cd0e4d
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2024-03-16 12:52:48 +0100

    Update erlpool_compile.erl

--- src/erlpool_compile.erl
+++ src/erlpool_compile.erl
@@ -9,7 +9,7 @@
 compile_settings(SettingsList) ->
     Forms = get_settings_code(SettingsList),
     %% for deubgging only
-    % file:write_file("/tmp/erlpool_globals_gen.erl", erl_prettypr:format(erl_syntax:form_list(Forms))),
+    file:write_file("/tmp/erlpool_globals_gen.erl", erl_prettypr:format(erl_syntax:form_list(Forms))),
     case merl:compile_and_load(Forms) of
         {ok, _Bin} ->
             ok;
